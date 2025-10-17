c92877372bb10e5b592456fd243c9d845bc0aeb0
KYuan <angusyuankn@gmail.com>
2025-10-17 18:42:59 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/clamart/',
   plugins: [
     vue(),
     // ElementPlus按需导入
